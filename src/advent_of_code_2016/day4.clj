c9cd0777f4713438e7bad20da19dd12744faec23
Nathan Armstrong <kenogu.labz@gmail.com>
2016-12-03 22:17:16 -0800

    Refactor Day 4.
    Pipelines everywhere, naming adjustments.

--- src/advent_of_code_2016/day4.clj
+++ src/advent_of_code_2016/day4.clj
@@ -1,35 +1,46 @@
 (ns advent-of-code-2016.day4
-  (:require [advent-of-code-2016.core :as ad-core]
+  (:require [advent-of-code-2016.core :refer [parse-int]]
             [clojure.string :as str]
             [clojure.java.io :as io]
             [clojure.set :as set]))
 
 (def problem-input (slurp (io/resource "day-4-input.txt")))
 
-(defn match-room
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(defn str->room
   [s]
   (let [[name id checksum] (rest (re-matches #"([a-z-]+)-(\d+)\[(\w{5})\]" s))]
     {:name name
-     :id (ad-core/parse-int id)
+     :id (parse-int id)
      :checksum checksum}))
 
-(defn get-name-freqs
-  [name]
-  (sort-by (juxt (comp - second) first)
-           (map #(vector (str (first %)) (second %))
-                (dissoc (frequencies name) \-))))
+(def compare-freq-desc (juxt (comp - second) first))
 
-(defn get-expected-checksum
+(defn letter-freqs
+  [s]
+  (->> s
+    (filter (set alphabet))
+    frequencies
+    (map #(update % 0 str))
+    (sort-by compare-freq-desc)))
+
+(defn expected-checksum
   [name]
-  (apply str (map first (take 5 (get-name-freqs name)))))
+  (->> name
+       get-letter-freqs
+       (take 5)
+       (map first)
+       (apply str)))
 
 (defn matches-checksum?
   [{:keys [name checksum]}]
   (= checksum (get-expected-checksum name)))
 
+;; Solution to Day 4-A
 (->> problem-input
      str/split-lines
-     (map match-room)
+     (map str->room)
      (filter matches-checksum?)
      (map :id)
      (reduce +)
@@ -38,22 +49,25 @@
 ;; Quick inspiration for Caesar cipher implementation from
 ;; https://clojurebridge.github.io/community-docs/docs/exercises/caesar-cipher/
 ;; - Thanks!
-(def alphabet "abcdefghijklmnopqrstuvwxyz")
-
 (defn cipher
   [shift-n]
-  (zipmap alphabet (take 26 (drop (mod shift-n 26) (cycle alphabet)))))
+  (->> alphabet
+       cycle
+       (drop (mod shift-n 26))
+       (take 26)
+       (zipmap alphabet)))
 
 (defn shift-by-id
   [{:keys [name id] :as room}]
-  (let [room-cipher (assoc (cipher id) \- \-)]
+  (let [room-cipher (assoc (cipher id) \- \space)]
     (assoc room :name (apply str (map room-cipher name)))))
 
+;; Solution to Day 4-B
 (->> problem-input
      str/split-lines
-     (map match-room)
+     (map str->room)
      (filter matches-checksum?)
      (map shift-by-id)
-     (map #(select-keys % [:name :id]))
      (filter #(re-matches #".+(pole).+" (:name %)))
+     (map :id)
      prn)
