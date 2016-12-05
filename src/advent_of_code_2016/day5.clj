8f835928172ba46da84209109612d2b556adcd5c
Nathan Armstrong <kenogu.labz@gmail.com>
2016-12-05 01:39:30 -0800

    Refactor Day 5.
    Yikes, this needed some TLC.
    - Part B is no longer solved with brute force and manual parsing.
    - The hashing itself has been tuned somewhat to cull hashes early, before converting them to a hex-formatted string.

--- src/advent_of_code_2016/day5.clj
+++ src/advent_of_code_2016/day5.clj
@@ -1,43 +1,88 @@
 (ns advent-of-code-2016.day5
-  (:require [advent-of-code-2016.core :refer [parse-int]]
-            [clojure.string :as str]
-            [clojure.java.io :as io]
-            [clojure.set :as set])
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   (:import [java.security MessageDigest]
            [java.math BigInteger]
-           [java.util Arrays]
-           [java.nio ByteBuffer])
-  (:gen-class))
+           [java.nio ByteBuffer]))
 
 (def problem-input (slurp (io/resource "day-5-input.txt")))
 
+;; Code for calculating an MD5 hash pulled from:
 ;; https://gist.github.com/jizhang/4325757
-(defn valid-hash [s]
-  (let [algorithm (MessageDigest/getInstance "MD5")
-        size (* 2 (.getDigestLength algorithm))
-        raw (.digest algorithm (.getBytes s))
-        [byte-1 byte-2 byte-3] raw]
-    (when (and (= byte-1 byte-2 0)
-               (>= byte-3 0)
-               (<= byte-3 15))
-      (let [sig (.toString (BigInteger. 1 raw) 16)
-            padding (apply str (repeat (- size (count sig)) "0"))]
-        (apply str (take 7 (str padding sig)))))))
+;; Thank you!
 
-;; Day 5-A
+(def md5-algorithm (MessageDigest/getInstance "MD5"))
+(def digest-size (* 2 (.getDigestLength md5-algorithm)))
+
+(defn raw-md5
+  "Gets the raw MD5 digest bytes for the given string."
+  [s]
+  (.digest md5-algorithm (.getBytes s)))
+
+(defn valid-hash?
+  "Returns whether the hash is valid (for our purposes),
+  i.e. begins with a sequence of five zero nybbles."
+  [[byte-1 byte-2 byte-3]]
+  (and (= byte-1 byte-2 0)
+       (>= byte-3 0)
+       (<= byte-3 15)))
+
+(defn hex-rep
+  "Converts the given hash into a hexidecimal string representation."
+  [hash]
+  (let [sig (.toString (BigInteger. 1 hash) 16)
+        padding (apply str (repeat (- digest-size (count sig)) "0"))]
+    (str padding sig)))
+
+;; Day 5-A Solution
 (->> (range)
      (map #(str problem-input %))
-     (map valid-hash)
-     (filter (complement nil?))
+     (map raw-md5)
+     (filter valid-hash?)
      (take 8)
+     (map hex-rep)
      (map #(nth % 5))
-     (apply str))
+     (apply str)
+     println)
+
+(defn pos-val-nybbles
+  "Returns the position and value nybbles from a given hash."
+  [[_ _ _ _ _ position value]]
+  {:position (- (int position) (int \0))
+   :value value})
+
+(def valid-positions (set (range 0 8)))
+
+(defn valid-position?
+  "Returns whether the given hash position is a valid
+  password position."
+  [{:keys [position]}]
+  (valid-positions position))
+
+(def empty-pass (vec (repeat 8 nil)))
+
+(defn insert-pass-char
+  "Inserts the given hash position-value pair into the given
+  password, or returns the password untouched if the position
+  has already been filled in."
+  [pass {:keys [position value]}]
+  (if (nil? (nth pass position))
+    (assoc pass position value)
+    pass))
+
+(defn pass-complete?
+  "Returns whether the password has all its slots filled."
+  [pass]
+  (every? (complement nil?) pass))
 
 ;; Day 5-B
 (->> (range)
-     (map #(str problem-input %))
-     (map valid-hash)
-     (filter (complement nil?))
-     (filter #((set "01234567") (nth % 5)))
-     (map #(take 2 (drop 5 %)))
-     (take 20))
+     (map (comp raw-md5 #(str problem-input %)))
+     (filter valid-hash?)
+     (map (comp pos-val-nybbles hex-rep))
+     (filter valid-position?)
+     (reductions insert-pass-char empty-pass)
+     (filter pass-complete?)
+     first
+     (apply str)
+     println)
