8681652060db2fb319dad66687b6e63536731681
Nathan Armstrong <kenogu.labz@gmail.com>
2016-12-06 22:04:05 -0800

    Refactor and tidy cruft in Day 7.

--- src/advent_of_code_2016/day7.clj
+++ src/advent_of_code_2016/day7.clj
@@ -1,37 +1,60 @@
 (ns advent-of-code-2016.day7
   (:require [advent-of-code-2016.core :as ad-core :refer [parse-int]]
             [clojure.string :as str]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.set :as set]))
 
 (def problem-input (slurp (io/resource "day-7-input.txt")))
 
+(defn is-abba?
+  [[a b c d]]
+  (and (not= a b)
+       (= b c)
+       (= a d)))
+
 (defn has-abba?
   [s]
-  (some (fn [[a b c d]] (and (not= a b) (= b c) (= a d))) (partition 4 1 s)))
-
-(re-seq #".*(\w)(\w)\2\1.*" "aaaabba")
-
-(has-abba? "aaaa")
+  (some is-abba? (partition 4 1 s)))
 
 (defn strip-hypernet
   [ip]
   (->> ip
     (re-seq #"(\w*)(?:\[\w*?\])?")
     (map second)
-    (filter (complement nil?))
+    (filter #(not (or (nil? %) (empty? %))))
     (filter (complement empty?))))
-
-(strip-hypernet "piftqfdhtumcmjmsge[qrsntvxhtfurcgcynx]oyswvuklvtmivlhen[syqhqtijyiduoxb]pdtdrhijqqzvcnl[xivmeqcwyafxvnok]jvlbkrwbgcgzaqms")
 
 (defn only-hypernet
   [ip]
-  (re-seq #"\[(\w*)\]" ip))
+  (map second (re-seq #"\[(\w*)\]" ip)))
 
 (defn supports-tls?
   [ip]
   (and (some has-abba? (strip-hypernet ip))
-       (not (some has-abba? (only-hypernet ip)))))
+       (every? (complement has-abba?) (only-hypernet ip))))
 
-(map supports-tls? ["abba[mnop]qrst"
-                    "abcd[bddb]xyyx"
-                    "aaaa[qwer]tyui"])
+(defn aba?
+  [[a b c]]
+  (and (= a c) (not= a b)))
+
+(defn flip [[a b]] [b a])
+
+(defn abas
+  [s]
+  (->> s
+       (partition 3 1)
+       (filter aba?)
+       (map #(take 2 %))))
+
+(defn supports-ssl?
+  [ip]
+  (let [supernet-abas (->> ip strip-hypernet (mapcat abas) set)
+        hypernet-babs (->> ip only-hypernet (mapcat abas) (map flip) set)]
+    (not (empty? (set/intersection supernet-abas hypernet-babs)))))
+
+;; Part A + B
+(->> problem-input
+     str/split-lines
+     ((juxt #(filter supports-tls? %) #(filter supports-ssl? %)))
+     (map count)
+     (map (partial str) ["Supporting TLS: " "Supporting SSL: "]))
